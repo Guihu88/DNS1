fcdee7aa01acc0f66406fcb0e504687dbdcb4703
Guihu88 <137459673+Guihu88@users.noreply.github.com>
2023-07-14 10:38:28 +0800

    Create dns.sh

---
+++ dns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 备份原始配置文件
+cp /etc/network/interfaces.d/50-cloud-init /etc/network/interfaces.d/50-cloud-init.bak
+
+# 新的DNS服务器地址
+new_dns_servers="61.19.42.5"
+
+# 替换dns-nameservers行
+sed -i "/^dns-nameservers/c\dns-nameservers $new_dns_servers" /etc/network/interfaces.d/50-cloud-init
+
+# 重启网络服务
+systemctl restart networking
