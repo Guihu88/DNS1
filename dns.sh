a85e4e79459506f1aac9c36ba1401b96d38a919c
Guihu88 <137459673+Guihu88@users.noreply.github.com>
2023-07-14 11:39:22 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -5,25 +5,19 @@ config_file="/etc/network/interfaces.d/50-cloud-init"
 backup_file="${config_file}.bak"
 cp "$config_file" "$backup_file"
 
-# 设置文件所有者和权限
-chown root:root "$config_file"
-chmod 644 "$config_file"
-
 # 新的DNS服务器地址
 new_dns_servers="61.19.42.5"
 
 # 替换dns-nameservers行
-if grep -q "dns-nameservers" "$config_file"; then
-    sed -i "s/^dns-nameservers.*/dns-nameservers $new_dns_servers/" "$config_file"
-else
-    echo "dns-nameservers $new_dns_servers" >> "$config_file"
-fi
+sed -i "s/^dns-nameservers .*/dns-nameservers $new_dns_servers/" "$config_file"
 
 # 重启网络服务
 systemctl restart networking
 
+# 等待网络服务重启完成
+sleep 5
+
 # 检查网络连接
-sleep 5  # 等待网络服务重新启动
 ping -c 3 google.com >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "DNS更换成功并且网络连接正常。"
