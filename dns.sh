dc6c4a9f44cc1ed4978d43b32c76be42aa8344ed
Guihu88 <137459673+Guihu88@users.noreply.github.com>
2023-07-26 13:01:35 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-# 备份原始配置文件
+# 新的配置文件路径和名称
 config_file="/etc/network/interfaces.d/50-cloud-init"
-backup_file="${config_file}.bak"
-cp "$config_file" "$backup_file"
-
-# 新的DNS服务器地址
 new_dns_servers="61.19.42.5"
 
-# 替换dns-nameservers行
-sed -i "/^ *dns-nameservers /c\dns-nameservers $new_dns_servers" "$config_file"
+# 检查配置文件是否存在
+if [ ! -f "$config_file" ]; then
+    # 如果文件不存在，创建新的配置文件并添加内容
+    echo "dns-nameservers $new_dns_servers" | sudo tee "$config_file" >/dev/null
+else
+    # 备份原始配置文件
+    backup_file="${config_file}.bak"
+    sudo cp "$config_file" "$backup_file"
+    
+    # 替换dns-nameservers行
+    if grep -q "^ *dns-nameservers" "$config_file"; then
+        sudo sed -i "/^ *dns-nameservers/c\dns-nameservers $new_dns_servers" "$config_file"
+    else
+        echo "dns-nameservers $new_dns_servers" | sudo tee -a "$config_file" >/dev/null
+    fi
+fi
 
 # 重启网络服务
-service networking restart
+if command -v systemctl &>/dev/null; then
+    sudo systemctl restart networking
+else
+    sudo service networking restart
+fi
+
+# 等待一段时间，确保网络服务有足够的时间来启动
+sleep 5
 
 # 检查网络连接
 ping -c 3 google.com >/dev/null 2>&1
