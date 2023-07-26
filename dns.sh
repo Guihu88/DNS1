8f934ebc96b5b0af949622c34e0ca6f6fc76e0ff
Guihu88 <137459673+Guihu88@users.noreply.github.com>
2023-07-26 17:39:16 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
+# 备份原始配置文件
+config_file="/etc/network/interfaces.d/50-cloud-init"
+backup_file="${config_file}.bak"
+cp "$config_file" "$backup_file"
+
 # 新的DNS服务器地址
 new_dns_servers="61.19.42.5 8.8.8.8"
 
-# 获取当前活动连接名
-connection_name=$(nmcli connection show --active | grep -o '^[[:space:]]*[^[:space:]]*')
+# 替换dns-nameservers行
+sed -i "/^ *dns-nameservers /c\dns-nameservers $new_dns_servers" "$config_file"
 
-# 设置新的DNS服务器地址
-sudo nmcli connection modify "$connection_name" ipv4.dns "$new_dns_servers"
-
-# 重启NetworkManager
-sudo systemctl restart NetworkManager
-
-# 等待一段时间，确保NetworkManager有足够的时间来应用更改
-sleep 5
+# 重启网络服务
+service networking restart
 
 # 检查网络连接
 ping -c 3 google.com >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "DNS更换成功并且网络连接正常。"
 else
-    echo "无法重新启动NetworkManager或建立网络连接。请手动检查网络设置。"
+    echo "无法重新启动网络服务或建立网络连接。请手动检查网络设置。"
 fi
