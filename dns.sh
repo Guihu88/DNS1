c0cc906a0ef5e781157a675ee54cde83bc81c173
Guihu88 <137459673+Guihu88@users.noreply.github.com>
2023-08-12 22:58:11 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -1,43 +1,39 @@
 #!/bin/bash
 
-# 创建自定义DNS配置文件路径
-custom_dns_path="/etc/custom_dns"
-custom_dns_file="$custom_dns_path/custom_dns.conf"
+# 备份原始配置文件
+config_file="/etc/network/interfaces.d/50-cloud-init"
+backup_file="${config_file}.bak"
+cp "$config_file" "$backup_file"
 
-# 创建自定义DNS配置文件
-mkdir -p "$custom_dns_path"
-echo "nameserver 8.8.8.8" > "$custom_dns_file"  # 默认的DNS服务器地址
-echo "自定义DNS配置文件创建成功。"
-
-# 自动获取国家代码
+# 新的DNS服务器地址根据国家
 country_code=$(curl -s ipinfo.io/country)
 case $country_code in
     TH)
-        echo "nameserver 61.19.42.5" >> "$custom_dns_file"
-        echo "nameserver 8.8.8.8" >> "$custom_dns_file"
+        new_dns_servers="61.19.42.5 8.8.8.8"
         ;;
     PH)
-        echo "nameserver 121.58.203.4" >> "$custom_dns_file"
-        echo "nameserver 8.8.8.8" >> "$custom_dns_file"
+        new_dns_servers="121.58.203.4 8.8.8.8"
         ;;
     MY)
-        echo "nameserver 49.236.193.35" >> "$custom_dns_file"
-        echo "nameserver 203.176.144.12" >> "$custom_dns_file"
-        echo "nameserver 8.8.8.8" >> "$custom_dns_file"
+        new_dns_servers="49.236.193.35 203.176.144.12 8.8.8.8"
         ;;
     ID)
-        echo "nameserver 8.8.8.8" >> "$custom_dns_file"
+        new_dns_servers="8.8.8.8"
+        ;;
+    *)
+        new_dns_servers="默认的DNS服务器地址"
         ;;
 esac
 
-# 获取第一个活动的网络连接名称
-active_connection=$(nmcli con show --active | awk 'NR==2{print $1}')
+# 替换dns-nameservers行
+sed -i "/^ *dns-nameservers /c\dns-nameservers $new_dns_servers" "$config_file"
 
-# 配置网络连接使用自定义DNS服务器
-if [ -n "$active_connection" ]; then
-    nmcli con mod "$active_connection" ipv4.dns "$custom_dns_file"
-    nmcli con up "$active_connection"
-    echo "网络接口 '$active_connection' 配置为使用自定义DNS服务器。"
+# 重启网络服务
+service networking restart
+
+# 检查网络连接
+if ping -c 3 google.com >/dev/null 2>&1; then
+    echo "DNS更换成功并且网络连接正常。"
 else
-    echo "未找到活动的网络连接。请手动检查网络连接。"
+    echo "无法重新启动网络服务或建立网络连接。请手动检查网络设置。"
 fi
