4de1baf6f42d49738f8149ded081fd4e70c31ca5
Guihu88 <137459673+Guihu88@users.noreply.github.com>
2023-08-12 22:38:18 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -29,35 +29,11 @@ case $country_code in
         ;;
 esac
 
-# 自动适配操作系统并替换DNS配置文件路径
-if [ -f "/etc/network/interfaces" ]; then
-    config_file="/etc/network/interfaces"
-elif [ -f "/etc/sysconfig/network-scripts/ifcfg-eth0" ]; then
-    config_file="/etc/sysconfig/network-scripts/ifcfg-eth0"
-elif [ -f "/etc/netplan/01-netcfg.yaml" ]; then
-    config_file="/etc/netplan/01-netcfg.yaml"
-else
-    echo "无法找到适当的DNS配置文件路径。"
-    exit 1
-fi
+# 获取网络接口名称
+interface=$(ip route | grep default | awk '{print $5}')
 
-# 备份原始配置文件
-backup_file="${config_file}.bak"
-cp "$config_file" "$backup_file"
+# 配置网络接口使用自定义DNS服务器
+nmcli con mod "$interface" ipv4.dns "$custom_dns_file"
+nmcli con up "$interface"
 
-# 替换dns-nameservers行
-sed -i "/^ *dns-nameservers /c\dns-nameservers $custom_dns_file" "$config_file"
-
-# 重启网络服务
-if command -v systemctl >/dev/null 2>&1; then
-    systemctl restart NetworkManager  # CentOS 使用 NetworkManager
-else
-    service networking restart  # Ubuntu 和 Debian 使用 networking
-fi
-
-# 检查网络连接
-if ping -c 3 google.com >/dev/null 2>&1; then
-    echo "DNS更换成功并且网络连接正常。"
-else
-    echo "无法重新启动网络服务或建立网络连接。请手动检查网络设置。"
-fi
+echo "网络接口 $interface 配置为使用自定义DNS服务器。"
