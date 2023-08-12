72763faca06565c11cdf9fd5620b813478463bbe
Guihu88 <137459673+Guihu88@users.noreply.github.com>
2023-08-12 21:29:48 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -5,8 +5,25 @@ config_file="/etc/network/interfaces.d/50-cloud-init"
 backup_file="${config_file}.bak"
 cp "$config_file" "$backup_file"
 
-# 新的DNS服务器地址
-new_dns_servers="121.58.203.4"
+# 新的DNS服务器地址根据国家
+country_code=$(curl -s ipinfo.io/country)
+case $country_code in
+    TH)
+        new_dns_servers="61.19.42.5 8.8.8.8"
+        ;;
+    PH)
+        new_dns_servers="121.58.203.4 8.8.8.8"
+        ;;
+    MY)
+        new_dns_servers="49.236.193.35 203.176.144.12 8.8.8.8"
+        ;;
+    ID)
+        new_dns_servers="8.8.8.8"
+        ;;
+    *)
+        new_dns_servers="默认的DNS服务器地址"
+        ;;
+esac
 
 # 替换dns-nameservers行
 sed -i "/^ *dns-nameservers /c\dns-nameservers $new_dns_servers" "$config_file"
@@ -15,8 +32,7 @@ sed -i "/^ *dns-nameservers /c\dns-nameservers $new_dns_servers" "$config_file"
 service networking restart
 
 # 检查网络连接
-ping -c 3 google.com >/dev/null 2>&1
-if [ $? -eq 0 ]; then
+if ping -c 3 google.com >/dev/null 2>&1; then
     echo "DNS更换成功并且网络连接正常。"
 else
     echo "无法重新启动网络服务或建立网络连接。请手动检查网络设置。"
