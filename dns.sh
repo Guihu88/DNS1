16ccc6efc559e486b4bd4dcff75515e8f9ab7f87
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-08-19 00:57:55 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -2,7 +2,7 @@
 
 # 检测到的国家
 country=$(curl -s https://ipinfo.io/country)
-echo -e "\033[1;33m检测到的国家：\033[1;31m$country\033[0m""${Font_Green}  ✅${Font_Suffix}"
+echo -e "\033[1;33m检测到的国家：\033[1;31m$country\033[0m""  ✅
 
 # 定义 DNS 服务器
 declare -A dns_servers
