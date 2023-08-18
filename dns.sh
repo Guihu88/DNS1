f6995910efc87fd2157725b7cf88c75227809bb3
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-08-18 23:43:21 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -2,7 +2,7 @@
 
 # 检测到的国家
 country=$(curl -s https://ipinfo.io/country)
-echo -e "\033[1;33m检测到的国家：$country\033[0m"
+echo -e "\033[1;32m检测到的国家：$country\033[0m"
 
 # 定义 DNS 服务器
 declare -A dns_servers
@@ -22,12 +22,9 @@ dns_servers=(
 
 # 方案一：修改 /etc/resolv.conf
 update_resolv_conf() {
-    echo "清空原有 DNS 设置"
-    echo -n | sudo tee /etc/resolv.conf
-
     echo "设置 DNS 服务器"
     for dns_server in ${dns_servers[$country]}; do
-        echo "nameserver $dns_server" | sudo tee -a /etc/resolv.conf
+        echo -e "\033[1;32mnameserver $dns_server\033[0m" | sudo tee -a /etc/resolv.conf
     done
 }
 
@@ -36,19 +33,14 @@ update_interfaces() {
     if grep -q "dns-nameservers" /etc/network/interfaces.d/50-cloud-init; then
         sudo sed -i '/dns-nameservers/d' /etc/network/interfaces.d/50-cloud-init
         echo -e "\033[1;32m修改 /etc/network/interfaces.d/50-cloud-init 成功。\033[0m"
-    else
-        echo -e "\033[1;31m修改 /etc/network/interfaces.d/50-cloud-init 失败。\033[0m"
     fi
 }
 
 # 清除 DNS 缓存函数
 flush_dns_cache() {
-    echo "清除 DNS 缓存..."
-    sudo systemd-resolve --flush-caches
+    sudo systemd-resolve --flush-caches 2>/dev/null
     if [ $? -eq 0 ]; then
-        echo -e "\033[1;32mDNS 缓存已清除。\033[0m"
-    else
-        echo -e "\033[1;31m清除 DNS 缓存失败。\033[0m"
+        echo -e "\033[1;32m清除 DNS 缓存成功。\033[0m"
     fi
 }
 
@@ -65,21 +57,21 @@ main() {
             ;;
     esac
 
-    # 如果方案一更新失败，执行方案二
+    # 方案一更新失败，执行方案二
     if [ $? -ne 0 ]; then
         update_interfaces
     fi
 
     # 检查是否有成功的更新
     if [ $? -eq 0 ]; then
-        echo -e "\033[1;32m更新 DNS 成功。\033[0m"
+        echo -e "\033[1;32m修改DNS成功。\033[0m"
     else
         echo -e "\033[1;31m任务失败。\033[0m"
     fi
 
-    echo -e "\033[1;33m定制IPLC线路：\033[1;32m广港、沪日、沪美、京德\033[0m"
-    echo -e "\033[1;33m定制TIKTOK网络：\033[1;32m美国、泰国、越南、菲律宾等\033[0m"
-    echo -e "\033[1;31m如有问题，请联系我：\033[1;33m联系方式TG:rocloudcc\033[0m"
+    echo -e "\033[1;32m定制IPLC线路：\033[1;32m广港、沪日、沪美、京德\033[0m"
+    echo -e "\033[1;32m定制TIKTOK网络：\033[1;32m美国、泰国、越南、菲律宾等\033[0m"
+    echo -e "\033[1;33m如有问题，请联系我：\033[1;32m联系方式TG:rocloudcc\033[0m"
     echo -e "\033[1;32m检测完成。\033[0m"
 }
 
