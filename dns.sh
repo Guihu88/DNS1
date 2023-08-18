55ca9408549e6bcd40456e69e526398ab829fef4
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-08-18 23:19:51 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -2,22 +2,22 @@
 
 # 检测到的国家
 country=$(curl -s https://ipinfo.io/country)
-echo -e "\e[1;33m检测到的国家：$country\e[0m"
+echo "检测到的国家：$country"
 
 # 定义 DNS 服务器
 declare -A dns_servers
 dns_servers=(
-    ["PH"]="\e[32m121.58.203.4 8.8.8.8\e[0m"
-    ["VN"]="\e[32m183.91.184.14 8.8.8.8\e[0m"
-    ["MY"]="\e[32m49.236.193.35 8.8.8.8\e[0m"
-    ["TH"]="\e[32m61.19.42.5 8.8.8.8\e[0m"
-    ["ID"]="\e[32m202.146.128.3 202.146.128.7 202.146.131.12\e[0m"
-    ["TW"]="\e[32m168.95.1.1 8.8.8.8\e[0m"
-    ["CN"]="\e[32m111.202.100.123 101.95.120.109 101.95.120.106\e[0m"
-    ["HK"]="\e[32m1.1.1.1 8.8.8.8\e[0m"
-    ["JP"]="\e[32m133.242.1.1 133.242.1.2\e[0m"
-    ["US"]="\e[32m1.1.1.1 8.8.8.8\e[0m"
-    ["DE"]="\e[32m217.172.224.47 194.150.168.168\e[0m"
+    ["PH"]="121.58.203.4 8.8.8.8"
+    ["VN"]="183.91.184.14 8.8.8.8"
+    ["MY"]="49.236.193.35 8.8.8.8"
+    ["TH"]="61.19.42.5 8.8.8.8"
+    ["ID"]="202.146.128.3 202.146.128.7 202.146.131.12"
+    ["TW"]="168.95.1.1 8.8.8.8"
+    ["CN"]="111.202.100.123 101.95.120.109 101.95.120.106"
+    ["HK"]="1.1.1.1 8.8.8.8"
+    ["JP"]="133.242.1.1 133.242.1.2"
+    ["US"]="1.1.1.1 8.8.8.8"
+    ["DE"]="217.172.224.47 194.150.168.168"
 )
 
 # 获取本机的公共 IP 地址
@@ -34,9 +34,16 @@ set_dns_servers() {
     done
 
     if [ $? -eq 0 ]; then
-        echo -e "\e[32mDNS 设置已成功更新。\e[0m"
+        echo "DNS 设置已成功更新。"
     else
-        echo -e "\e[31m更新 DNS 设置失败。\e[0m"
+        echo "更新 DNS 设置失败。"
+        # 尝试修改 /etc/network/interfaces.d/50-cloud-init
+        if grep -q "dns-nameservers" /etc/network/interfaces.d/50-cloud-init; then
+            sudo sed -i '/dns-nameservers/d' /etc/network/interfaces.d/50-cloud-init
+            echo "修改 /etc/network/interfaces.d/50-cloud-init 成功。"
+        else
+            echo "修改 /etc/network/interfaces.d/50-cloud-init 失败。"
+        fi
     fi
 }
 
@@ -45,9 +52,9 @@ flush_dns_cache() {
     echo "清除 DNS 缓存..."
     sudo systemd-resolve --flush-caches
     if [ $? -eq 0 ]; then
-        echo -e "\e[32mDNS 缓存已清除。\e[0m"
+        echo "DNS 缓存已清除。"
     else
-        echo -e "\e[31m清除 DNS 缓存失败。\e[0m"
+        echo "清除 DNS 缓存失败。"
     fi
 }
 
@@ -59,15 +66,15 @@ main() {
             flush_dns_cache
             ;;
         *)
-            echo -e "\e[31m未识别的国家或不在列表中。\e[0m"
+            echo "未识别的国家或不在列表中。"
             exit 1
             ;;
     esac
 
-    echo -e "\e[1;33m定制IPLC线路：\e[0m\e[32m广港、沪日、沪美、京德\e[0m"
-    echo -e "\e[1;33m定制TIKTOK网络：\e[0m\e[32m美国、泰国、越南、菲律宾等\e[0m"
-    echo -e "\e[1;31m如有问题，请联系我：\e[0m\e[1;33m联系方式TG:rocloudcc\e[0m"
-    echo -e "\e[32m检测完成。\e[0m"
+    echo -e "\033[1;33m定制IPLC线路：广港、沪日、沪美、京德\033[0m"
+    echo -e "\033[1;33m定制TIKTOK网络：美国、泰国、越南、菲律宾等\033[0m"
+    echo -e "\033[1;31m如有问题，请联系我：联系方式TG:rocloudcc\033[0m"
+    echo -e "\033[1;32m检测完成。\033[0m"
 }
 
 # 执行主函数
