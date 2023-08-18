ca89443ed33d256d535965a54c4c53afde38be70
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-08-18 23:34:31 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -2,7 +2,7 @@
 
 # 检测到的国家
 country=$(curl -s https://ipinfo.io/country)
-echo "检测到的国家：$country"
+echo -e "\033[1;33m检测到的国家：$country\033[0m"
 
 # 定义 DNS 服务器
 declare -A dns_servers
@@ -20,11 +20,8 @@ dns_servers=(
     ["DE"]="217.172.224.47 194.150.168.168"
 )
 
-# 获取本机的公共 IP 地址
-public_ip=$(curl -s https://api64.ipify.org)
-
-# 设置 DNS 服务器函数
-set_dns_servers() {
+# 方案一：修改 /etc/resolv.conf
+update_resolv_conf() {
     echo "清空原有 DNS 设置"
     echo -n | sudo tee /etc/resolv.conf
 
@@ -32,18 +29,15 @@ set_dns_servers() {
     for dns_server in ${dns_servers[$country]}; do
         echo "nameserver $dns_server" | sudo tee -a /etc/resolv.conf
     done
+}
 
-    if [ $? -eq 0 ]; then
-        echo "DNS 设置已成功更新。"
+# 方案二：修改 /etc/network/interfaces.d/50-cloud-init
+update_interfaces() {
+    if grep -q "dns-nameservers" /etc/network/interfaces.d/50-cloud-init; then
+        sudo sed -i '/dns-nameservers/d' /etc/network/interfaces.d/50-cloud-init
+        echo -e "\033[1;32m修改 /etc/network/interfaces.d/50-cloud-init 成功。\033[0m"
     else
-        echo -e "\033[1;31m更新 DNS 设置失败。\033[0m"
-        # 尝试修改 /etc/network/interfaces.d/50-cloud-init
-        if grep -q "dns-nameservers" /etc/network/interfaces.d/50-cloud-init; then
-            sudo sed -i '/dns-nameservers/d' /etc/network/interfaces.d/50-cloud-init
-            echo -e "\033[1;33m修改 /etc/network/interfaces.d/50-cloud-init 成功。\033[0m"
-        else
-            echo -e "\033[1;31m修改 /etc/network/interfaces.d/50-cloud-init 失败。\033[0m"
-        fi
+        echo -e "\033[1;31m修改 /etc/network/interfaces.d/50-cloud-init 失败。\033[0m"
     fi
 }
 
@@ -52,7 +46,7 @@ flush_dns_cache() {
     echo "清除 DNS 缓存..."
     sudo systemd-resolve --flush-caches
     if [ $? -eq 0 ]; then
-        echo "DNS 缓存已清除。"
+        echo -e "\033[1;32mDNS 缓存已清除。\033[0m"
     else
         echo -e "\033[1;31m清除 DNS 缓存失败。\033[0m"
     fi
@@ -62,7 +56,7 @@ flush_dns_cache() {
 main() {
     case $country in
         "PH"|"VN"|"MY"|"TH"|"ID"|"TW"|"CN"|"HK"|"JP"|"US"|"DE")
-            set_dns_servers
+            update_resolv_conf
             flush_dns_cache
             ;;
         *)
@@ -70,6 +64,18 @@ main() {
             exit 1
             ;;
     esac
+
+    # 如果方案一更新失败，执行方案二
+    if [ $? -ne 0 ]; then
+        update_interfaces
+    fi
+
+    # 检查是否有成功的更新
+    if [ $? -eq 0 ]; then
+        echo -e "\033[1;32m更新 DNS 成功。\033[0m"
+    else
+        echo -e "\033[1;31m任务失败。\033[0m"
+    fi
 
     echo -e "\033[1;33m定制IPLC线路：\033[1;32m广港、沪日、沪美、京德\033[0m"
     echo -e "\033[1;33m定制TIKTOK网络：\033[1;32m美国、泰国、越南、菲律宾等\033[0m"
