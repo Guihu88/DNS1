58702710af6b682bc3bb033e165cbf8f0ff451e2
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-08-19 16:25:35 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -2,7 +2,7 @@
 
 # 检测到的国家
 country=$(curl -s https://ipinfo.io/country)
-echo -e "\033[1;33m检测到的国家：\033[1;31m$country\033[0m ✅"
+echo -e "\n\n\033[1;33m检测到的国家：\033[1;31m$country\033[0m ✅"
 
 # 定义 DNS 服务器
 declare -A dns_servers
@@ -18,59 +18,42 @@ dns_servers=(
     ["JP"]="133.242.1.1 133.242.1.2"
     ["US"]="1.1.1.1 8.8.8.8"
     ["DE"]="217.172.224.47 194.150.168.168"
+    # ... 添加其他国家的DNS服务器
 )
 
+# 修改 /etc/resolv.conf
 update_resolv_conf() {
-    echo -e "\033[1;34m执行任务\033[0m"
-
-    # 备份配置文件
-    sudo cp /etc/resolv.conf /etc/resolv.conf.bak
-
-    # 清除配置文件内容
-    sudo sh -c "echo > /etc/resolv.conf"
-
-    # 添加新的 DNS 服务器
+    echo -e "\033[1;34m执行任务：修改 /etc/resolv.conf\033[0m"
     for dns_server in ${dns_servers[$country]}; do
-        echo "nameserver $dns_server" | sudo tee -a /etc/resolv.conf > /dev/null
+        echo "nameserver $dns_server" | su -c "tee /etc/resolv.conf.new"
     done
-
-    # 清除系统 DNS 缓存
-    flush_dns_cache
+    su -c "mv /etc/resolv.conf.new /etc/resolv.conf"
 }
 
-flush_dns_cache() {
-    sudo systemd-resolve --flush-caches 2>/dev/null
-    if [ $? -eq 0 ]; then
-        echo -e "\033[1;32m清除 DNS 缓存成功。\033[0m"
-    fi
-    echo -e ""
+# 重启 NetworkManager
+restart_network_manager() {
+    su -c "systemctl restart NetworkManager"
 }
 
+# 主函数
 main() {
     case $country in
         "PH"|"VN"|"MY"|"TH"|"ID"|"TW"|"CN"|"HK"|"JP"|"US"|"DE")
             update_resolv_conf
+            restart_network_manager
+            echo -e "\033[1;32m修改DNS成功，已重启NetworkManager。\033[0m"
+            echo -e "\n\n================================================\n"
+            echo -e "\033[3;33m定制IPLC线路：\033[1;32m广港、沪日、沪美、京德等\033[0m"
+            echo -e "\033[3;33m定制TIKTOK网络：\033[1;32m美国、泰国、越南、菲律宾等\033[0m"
+            echo -e "\n================================================\n\n"
+            echo -e "\033[1;32mNDS已成功更换成目标国家：\033[1;31m$country\033[0m ✅\n\n"
             ;;
         *)
             echo -e "\033[1;31m未识别的国家或不在列表中。\033[0m"
             exit 1
             ;;
     esac
-
-    echo -e ""
-    echo -e "================================================"
-    echo -e ""
-    echo -e "\033[3;33m定制IPLC线路：\033[1;32m广港、沪日、沪美、京德\033[0m"
-    echo -e "\033[3;33mTG群聊：\033[1;31mhttps://t.me/rocloudiplc\033[0m"
-    echo -e "\033[3;33m定制TIKTOK网络：\033[1;32m美国、泰国、越南、菲律宾等\033[0m"
-    echo -e "\033[1;33m如有问题，请联系我：\033[1;35m联系方式TG：rocloudcc\033[0m"
-    echo -e ""
-    echo -e "================================================"
-    echo -e ""
-    echo -e ""
-    echo -e "\033[1;32mNDS已成功更换成目标国家：\033[1;31m$country\033[0m ✅"
-    echo -e ""
-    echo -e ""
 }
 
+# 执行主函数
 main
