fc35125dc3e3dabec03999bc141ec3d9cbe10237
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-08-20 16:20:21 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -4,20 +4,20 @@ function MediaUnlockTest_Tiktok_Region() {
     local Ftmpresult=$(curl -s --max-time 10 "https://www.tiktok.com/")
 
     if [[ "$Ftmpresult" = "curl"* ]]; then
-        echo "Failed (Network Connection)"
+        echo "Failed"
         return
     fi
 
     local FRegion=$(echo $Ftmpresult | grep '"region":' | sed 's/.*"region"//' | cut -f2 -d'"')
     if [ -n "$FRegion" ]; then
-        echo "【${FRegion}】"
+        echo "${FRegion}"
         return
     fi
 
     local STmpresult=$(curl -sL --max-time 10 -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Accept-Encoding: gzip" -H "Accept-Language: en" "https://www.tiktok.com" | gunzip 2>/dev/null)
     local SRegion=$(echo $STmpresult | grep '"region":' | sed 's/.*"region"//' | cut -f2 -d'"')
     if [ -n "$SRegion" ]; then
-        echo "【${SRegion}】(可能为IDC IP)"
+        echo "${SRegion}"
         return
     else
         echo "Failed"
