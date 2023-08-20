c70f60ac450a004fdaacf2937dfc89c0f04b7b66
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-08-20 16:09:20 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
+Font_Green="\033[32m"
+Font_Yellow="\033[33m"
+Font_Red="\033[31m"
+Font_Suffix="\033[0m"
 
-# 获取 TikTok 返回的国家代码
-TiktokRegion=$(curl -s https://www.tiktok.com/ | grep -oP '(?<="region":")[A-Z]+')
+function MediaUnlockTest_Tiktok_Region() {
+    echo -n -e " Tiktok Region:\t\t\c"
+    local Ftmpresult=$(curl -s --max-time 10 "https://www.tiktok.com/")
 
-# 判断国家代码并输出对应国家
-case $TiktokRegion in
-    MY)
-        echo "马来西亚 (Malaysia)"
-        ;;
-    PH)
-        echo "菲律宾 (Philippines)"
-        ;;
-    *)
-        echo "未知地区"
-        ;;
-esac
+    if [[ "$Ftmpresult" = "curl"* ]]; then
+        echo -n -e "\r Tiktok Region:\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n"
+        return
+    fi
+
+    local FRegion=$(echo $Ftmpresult | grep '"region":' | sed 's/.*"region"//' | cut -f2 -d'"')
+    if [ -n "$FRegion" ]; then
+        echo -n -e "\r Tiktok Region:\t\t${Font_Green}【${FRegion}】${Font_Suffix}\n"
+        return
+    fi
+
+    local STmpresult=$(curl -sL --max-time 10 "https://www.tiktok.com" | gunzip 2>/dev/null)
+    local SRegion=$(echo $STmpresult | grep '"region":' | sed 's/.*"region"//' | cut -f2 -d'"')
+    if [ -n "$SRegion" ]; then
+        echo -n -e "\r Tiktok Region:\t\t${Font_Yellow}【${SRegion}】(可能为IDC IP)${Font_Suffix}\n"
+        return
+    else
+        echo -n -e "\r Tiktok Region:\t\t${Font_Red}Failed${Font_Suffix}\n"
+        return
+    fi
+}
+
+function RunScript() {
+    MediaUnlockTest_Tiktok_Region
+}
+
+RunScript
