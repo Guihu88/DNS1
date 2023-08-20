6242da7720c695545761ab5d1c4bd0dbf5cabebe
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-08-20 16:00:36 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-# 设置字体颜色
-Font_Red="\033[31m"
-Font_Green="\033[32m"
-Font_Yellow="\033[33m"
-Font_Suffix="\033[0m"
+# 使用curl获取TikTok页面内容
+tiktok_response=$(curl -s "https://www.tiktok.com/")
 
-# 获取IP地址
-local_ipv4=$(curl -4 -s --max-time 10 api64.ipify.org)
+# 提取国家代码（假设国家代码位于"region"字段中）
+country_code=$(echo "$tiktok_response" | grep -o -P '(?<="region": ")[A-Z]+')
 
-# 发送请求获取TikTok页面内容
-tiktok_page=$(curl -s --max-time 10 "https://www.tiktok.com/")
-
-# 从页面内容中提取区域信息
-region=$(echo $tiktok_page | grep -o '"region":"[^"]*' | sed 's/"region":"//')
-
-# 判断是否成功获取区域信息
-if [ -n "$region" ]; then
-    echo -e " Tiktok Region:\t\t${Font_Green}【${region}】${Font_Suffix}"
-else
-    echo -e " Tiktok Region:\t\t${Font_Red}Failed${Font_Suffix}"
-fi
+# 根据国家代码给出相应的归属地信息
+case $country_code in
+    MY)
+        echo "马来西亚"
+        ;;
+    SG)
+        echo "新加坡"
+        ;;
+    PH)
+        echo "菲律宾"
+        ;;
+    # 添加其他国家代码和相应的归属地信息
+    *)
+        echo "未知归属地"
+        ;;
+esac
