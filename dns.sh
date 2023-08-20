2c0df4a5f9aa9248633f89aa25cbf1502467cec2
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-08-20 16:05:34 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -1,24 +1,52 @@
 #!/bin/bash
+shopt -s expand_aliases
+Font_Black="\033[30m"
+Font_Red="\033[31m"
+Font_Green="\033[32m"
+Font_Yellow="\033[33m"
+Font_Blue="\033[34m"
+Font_Purple="\033[35m"
+Font_SkyBlue="\033[36m"
+Font_White="\033[37m"
+Font_Suffix="\033[0m"
 
-# 使用curl获取TikTok页面内容
-tiktok_response=$(curl -s "https://www.tiktok.com/")
+# ... （之前的变量和函数定义）
 
-# 提取国家代码（假设国家代码位于"region"字段中）
-country_code=$(echo "$tiktok_response" | grep -o -P '(?<="region": ")[A-Z]+')
+# 脚本开始
+clear
 
-# 根据国家代码给出相应的归属地信息
-case $country_code in
-    MY)
-        echo "马来西亚"
-        ;;
-    SG)
-        echo "新加坡"
-        ;;
-    PH)
-        echo "菲律宾"
-        ;;
-    # 添加其他国家代码和相应的归属地信息
-    *)
-        echo "未知归属地"
-        ;;
-esac
+# 检查操作系统
+checkOS() {
+    ifCentOS=$(cat /etc/os-release | grep CentOS)
+    if [ -n "$ifCentOS" ]; then
+        OS_Version=$(cat /etc/os-release | grep REDHAT_SUPPORT_PRODUCT_VERSION | cut -f2 -d'"')
+        if [[ "$OS_Version" -lt "8" ]]; then
+            echo -e "${Font_Red}此脚本不支持CentOS${OS_Version}，请升级至CentOS8或更换其他操作系统${Font_Suffix}"
+            echo -e "${Font_Red}3秒后退出脚本...${Font_Suffix}"
+            sleep 3
+            exit 1
+        fi
+    fi
+}
+checkOS
+
+# ... （之前的变量和函数定义）
+
+# 脚本标题
+function ScriptTitle() {
+    echo -e "${Font_SkyBlue}【Tiktok区域检测】${Font_Suffix}"
+    echo -e "${Font_Green}BUG反馈或使用交流可加TG群组${Font_Suffix} ${Font_Yellow}https://t.me/tiktok_operate ${Font_Suffix}"
+    echo ""
+    echo -e " ** 测试时间: $(date)"
+    echo ""
+}
+ScriptTitle
+
+# 运行脚本
+function RunScript() {
+    Heading
+    MediaUnlockTest_Tiktok_Region
+    Goodbye
+}
+
+RunScript
