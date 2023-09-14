b07a9d944877fb5b0f46ccb3ddcc0c8a4e0a6006
rocloudcc <137459673+rocloudcc@users.noreply.github.com>
2023-09-15 05:16:57 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -30,27 +30,19 @@ client_private_key=$(wg genkey)
 # 生成WireGuard客户端公钥
 client_public_key=$(echo "$client_private_key" | wg pubkey)
 
-# 将客户端公钥保存到文件
-echo "$client_public_key" > /etc/wireguard/client_public_key
-
 # 创建WireGuard客户端配置文件
 cat <<EOF > /etc/wireguard/client.conf
 [Interface]
 PrivateKey = $client_private_key
-Address = 10.77.0.2/24
+Address = 10.0.0.2/24
 DNS = 8.8.8.8
-MTU = 1380
 
 [Peer]
-PublicKey = <服务器的公钥>
+PublicKey = $(cat /etc/wireguard/wg0.conf | grep "PublicKey" | cut -d " " -f 3)
 AllowedIPs = 0.0.0.0/0, ::/0
-Endpoint = 101.36.126.206:51820
+Endpoint = $(curl ifconfig.me):51820
 PersistentKeepalive = 25
 EOF
-
-# 将服务器的公钥添加到客户端配置文件
-server_public_key=$(cat /etc/wireguard/wg0.conf | grep "PublicKey" | cut -d " " -f 3)
-sed -i "s/<服务器的公钥>/$server_public_key/g" /etc/wireguard/client.conf
 
 # 启用IPv4转发
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
@@ -65,4 +57,4 @@ systemctl enable wg-quick@wg0
 # 输出客户端配置文件
 cat /etc/wireguard/client.conf
 
-echo "WireGuard配置已完成。客户端配置文件位于 /etc/wireguard/client.conf。请将 <服务器的公钥> 替换为实际的服务器公钥。"
+echo "WireGuard配置已完成。客户端配置文件位于 /etc/wireguard/client.conf。"
